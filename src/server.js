c53aff38887d2c76c13f84d678ef57cb6547ff1d
Saul Dominguez <dguezs@gmail.com>
2025-03-06 16:35:26 -0600

    se suprimieron lineas deprecadas en la conexión a mongodb

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use("/uploads", express.static("uploads")); // Servir imágenes estáticamente
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB conectado"))
   .catch((err) => console.error("Error de conexión:", err));
 
